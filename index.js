422432b7fee48e582f94c4289c04e4764a018351
ashish <ashishvishwakarma4562@gmail.com>
2026-01-02 13:16:20 +0530

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 import app from "./app.js";
@@ -6,7 +7,14 @@ import ConnectDB from "./config/DB.js";
 import "./service/cron.js";
 
 const PORT = process.env.PORT || 5000;
-
+app.use(cors({
+  origin: ["http://localhost:5173",
+  "https://sunsirse-admin-fd.onrender.com",
+ 
+ ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
+}))
 ConnectDB(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => {
